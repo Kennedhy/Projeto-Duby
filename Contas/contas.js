048df1d44e44e9bfa98b72ac7016c25e87e659ba
Kennedhy <kennedhysantos28@gmail.com>
2025-05-22 19:15:19 -0300

    .

--- Contas/contas.js
+++ Contas/contas.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const botoesAbas = document.querySelectorAll('.botaoAba[data-relatorio]');
-    const conteudosRelatorios = document.querySelectorAll('.conteudoRelatorio');
+    const conteudosAbas = document.querySelectorAll('.conteudoAbas');
 
     botoesAbas.forEach(botao => {
         botao.addEventListener('click', function() {
-            const relatorioId = this.getAttribute('data-relatorio');
+            const abasId = this.getAttribute('data-relatorio');
             
             botoesAbas.forEach(b => b.classList.remove('ativo'));
-            conteudosRelatorios.forEach(c => c.classList.remove('ativo'));
+            conteudosAbas.forEach(c => c.classList.remove('ativo'));
             
             this.classList.add('ativo');
-            document.getElementById(relatorioId).classList.add('ativo');
+            document.getElementById(abasId).classList.add('ativo');
         });
     });
 
     if (!document.querySelector('.botaoAba.ativo')) {
         botoesAbas[0].classList.add('ativo');
-        conteudosRelatorios[0].classList.add('ativo');
+        conteudosAbas[0].classList.add('ativo');
     }
 });
 
