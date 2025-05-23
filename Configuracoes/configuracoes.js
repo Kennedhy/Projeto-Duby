fce1da11ae0e7d3172d0d3e41539b2b2abf1e664
Yarllei01 <alvesyarllei@gmail.com>
2025-05-23 16:42:59 -0300

    overlay cartão

--- Configuracoes/configuracoes.js
+++ Configuracoes/configuracoes.js
@@ -21,6 +21,28 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+document.getElementById('botao-adicionar-cartao').addEventListener('click', function () {
+  document.getElementById('fundo-modal').classList.remove('escondido');
+});
+
+document.getElementById('botao-cancelar').addEventListener('click', function () {
+  document.getElementById('fundo-modal').classList.add('escondido');
+});
+
+document.getElementById('formulario-cartao').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const numero = document.getElementById('numero-cartao').value;
+  const nome = document.getElementById('nome-titular').value;
+  const validade = document.getElementById('validade').value;
+  const cvv = document.getElementById('codigo-seguranca').value;
+
+  console.log('Cartão adicionado:', { numero, nome, validade, cvv });
+  this.reset();
+  document.getElementById('fundo-modal').classList.add('escondido');
+});
+
+
       document.querySelectorAll('.toggle-switch').forEach(toggle => {
         toggle.addEventListener('click', () => {
           toggle.classList.toggle('active');
